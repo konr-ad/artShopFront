2e5f118e81693c20914f753fd263856d438d7b75
Konrad <konradk95@gmail.com>
2024-05-31 22:55:52 +0200

    adding painting service

---
+++ src/app/services/painting.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Painting {
+  id?: number;
+  name: string;
+  type: string;
+  state: string;
+  length: number;
+  width: number;
+  price: number;
+  image: File;
+  imageUrl?: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaintingService {
+  private apiUrl = 'http://localhost:8080/api/paintings';
+
+  constructor(private http: HttpClient) {}
+
+  getPaintings(): Observable<Painting[]> {
+    return this.http.get<Painting[]>(this.apiUrl);
+  }
+
+  createPainting(painting: FormData): Observable<Painting> {
+    return this.http.post<Painting>(this.apiUrl, painting);
+  }
+}
