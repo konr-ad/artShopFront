b3e7035d13abd3bd5dcfbc3a6b0bb97ba33b338a
Konrad <konradk95@gmail.com>
2024-06-01 20:36:54 +0200

    implementation of product-list component

---
+++ src/app/components/header/navbar/shop/product-list/product-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Painting, PaintingService } from 'src/app/services/painting.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit, OnChanges {
+  @Input() filters: any;
+  paintings: Painting[] = [];
+  filteredPaintings: Painting[] = [];
+
+  constructor(private paintingService: PaintingService) { }
+
+  ngOnInit(): void {
+    this.fetchPaintings();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['filters']) {
+      this.applyFilters();
+    }
+  }
+
+  fetchPaintings(): void {
+    this.paintingService.getPaintings().subscribe((data: Painting[]) => {
+      this.paintings = data.map(painting => ({
+        ...painting,
+        imageUrl: 'data:image/jpeg;base64,' + painting.image
+      }));
+      this.applyFilters();
+    });
+  }
+
+  applyFilters(): void {
+    if (!this.filters || !this.filters['paintings'] || !this.filters['paintings'].length) {
+      this.filteredPaintings = this.paintings;
+    } else {
+      this.filteredPaintings = this.paintings.filter(painting =>
+        this.filters['paintings'].includes(painting.type)
+      );
+    }
+  }
+}
