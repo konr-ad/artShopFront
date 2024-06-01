9ccccaa32707dabf58399d7e5818fd46c2909732
Konrad <konradk95@gmail.com>
2024-06-01 20:37:13 +0200

    tech changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ShopComponent } from "./components/header/navbar/shop/shop.component";
-import {BrowserModule} from "@angular/platform-browser";
+import { BrowserModule } from "@angular/platform-browser";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   // Add routes for other components here if needed
   { path: 'shop', component: ShopComponent },
+  { path: 'all', component: ShopComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
   declarations: [
-    ShopComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AdditionalElementsComponent } from './components/header/additional-elements/additional-elements.component';
 import { NavbarComponent } from './components/header/navbar/navbar.component';
 import { LogoComponent } from './components/header/logo/logo.component';
+import { FiltersComponent } from './components/header/navbar/shop/filters/filters.component';
+import { ProductListComponent } from './components/header/navbar/shop/product-list/product-list.component';
+import { ShopComponent } from './components/header/navbar/shop/shop.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,10 @@ import { LogoComponent } from './components/header/logo/logo.component';
     HeaderComponent,
     AdditionalElementsComponent,
     NavbarComponent,
-    LogoComponent
+    LogoComponent,
+    ShopComponent,
+    FiltersComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
