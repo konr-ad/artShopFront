a97f08dad6fc5a3dd4a164dd9030d72611653999
Konrad <konradk95@gmail.com>
2024-07-29 20:44:33 +0200

    Added conditions to display header on an every page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  showHeader: boolean = true;
   title = 'paintings-app';
+
+  constructor(private router: Router) {}
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const noHeaderRoutes = ['/login', '/admin'];
+        this.showHeader = !noHeaderRoutes.includes(event.urlAfterRedirects);
+      }
+    });
+  }
 }
