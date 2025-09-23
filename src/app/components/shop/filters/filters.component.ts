701c470d094b5de594c1dd54dd98636bf37cdb39
Konrad <konradk95@gmail.com>
2025-09-23 21:54:11 +0200

    improving ui of filters

--- src/app/components/shop/filters/filters.component.ts
+++ src/app/components/shop/filters/filters.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, ElementRef, HostListener, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -6,44 +6,62 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.css'],
 })
-export class FiltersComponent implements OnInit {
+export class FiltersComponent implements OnInit, OnDestroy {
   @Output() filtersChange = new EventEmitter<any>();
-  filters: any = {
-    paintings: [],
-    priceFrom: null,
-    priceTo: null,
-  };
-  paintingTypeDropdownOpen: boolean = false;
-  priceDropdownOpen: boolean = false;
+  filters: any = { paintings: [], priceFrom: null, priceTo: null };
+  paintingTypeDropdownOpen = false;
+  priceDropdownOpen = false;
 
-  constructor(private route: ActivatedRoute) {}
+  private hoverCloseTimer?: any;
+
+  constructor(private route: ActivatedRoute, private el: ElementRef<HTMLElement>) {}
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe((params) => {
+    this.route.queryParams.subscribe(params => {
       const type = params['type'];
       const priceFrom = params['priceFrom'];
       const priceTo = params['priceTo'];
 
-      if (type) {
-        this.filters.paintings = Array.isArray(type) ? type : [type];
-      } else {
-        this.filters.paintings = [];
-      }
-
+      this.filters.paintings = type ? (Array.isArray(type) ? type : [type]) : [];
       this.filters.priceFrom = priceFrom ? +priceFrom : null;
-      this.filters.priceTo = priceTo ? +priceTo : null;
+      this.filters.priceTo   = priceTo ? +priceTo   : null;
       this.applyFilters();
     });
   }
 
-  togglePaintingTypeDropdown(): void {
-    this.paintingTypeDropdownOpen = !this.paintingTypeDropdownOpen;
+  ngOnDestroy(): void {
+    if (this.hoverCloseTimer) clearTimeout(this.hoverCloseTimer);
   }
 
-  togglePriceDropdown(): void {
-    // Method to toggle price dropdown
-    this.priceDropdownOpen = !this.priceDropdownOpen;
+  // --- outside click / Esc
+  @HostListener('document:click', ['$event'])
+  onDocClick(ev: MouseEvent) {
+    if (!this.el.nativeElement.contains(ev.target as Node)) this.closeAll();
   }
+
+  @HostListener('document:keydown.escape')
+  onEsc() { this.closeAll(); }
+
+  private closeAll() {
+    this.paintingTypeDropdownOpen = false;
+    this.priceDropdownOpen = false;
+  }
+
+  // --- hover leave/enter (zamykanie po wyjechaniu myszą)
+  onDropdownEnter() {
+    if (this.hoverCloseTimer) {
+      clearTimeout(this.hoverCloseTimer);
+      this.hoverCloseTimer = undefined;
+    }
+  }
+
+  onDropdownLeave() {
+    this.hoverCloseTimer = setTimeout(() => this.closeAll(), 180);
+  }
+
+  // --- Twoje istniejące metody
+  togglePaintingTypeDropdown() { this.paintingTypeDropdownOpen = !this.paintingTypeDropdownOpen; }
+  togglePriceDropdown() { this.priceDropdownOpen = !this.priceDropdownOpen; }
 
   onFilterChange(filterType: string, event: any): void {
     const filterValue = event.target.value;
@@ -51,14 +69,10 @@ export class FiltersComponent implements OnInit {
       this.filters[filterType] = filterValue ? +filterValue : null;
     } else {
       if (event.target.checked) {
-        if (!this.filters[filterType].includes(filterValue)) {
-          this.filters[filterType].push(filterValue);
-        }
+        if (!this.filters[filterType].includes(filterValue)) this.filters[filterType].push(filterValue);
       } else {
-        const index = this.filters[filterType].indexOf(filterValue);
-        if (index > -1) {
-          this.filters[filterType].splice(index, 1);
-        }
+        const i = this.filters[filterType].indexOf(filterValue);
+        if (i > -1) this.filters[filterType].splice(i, 1);
       }
     }
     this.applyFilters();
@@ -68,21 +82,15 @@ export class FiltersComponent implements OnInit {
     return this.filters[filterType].includes(filterValue);
   }
 
-  applyFilters(): void {
-    this.filtersChange.emit({ ...this.filters });
-  }
+  applyFilters(): void { this.filtersChange.emit({ ...this.filters }); }
 
   resetFilters(): void {
-    this.filters = {
-      paintings: [],
-      priceFrom: null,
-      priceTo: null,
-    };
-    this.filtersChange.emit({ ...this.filters });
+    this.filters = { paintings: [], priceFrom: null, priceTo: null };
+    this.applyFilters();
   }
 
   onInputChange(filterType: string, event: Event): void {
-    const inputElement = event.target as HTMLInputElement;
-    this.onFilterChange(filterType, { target: inputElement });
+    const input = event.target as HTMLInputElement;
+    this.onFilterChange(filterType, { target: input });
   }
 }
