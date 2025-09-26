cae5f2827e61a4ebdd66de36b6d0e5f252b59e4e
Konrad <konradk95@gmail.com>
2025-09-26 12:07:17 +0200

    smooth scrolling on terms component

--- src/app/components/terms/terms.component.ts
+++ src/app/components/terms/terms.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {TranslatePipe} from "@ngx-translate/core";
-
+import { ViewportScroller } from '@angular/common';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs/operators";
 @Component({
   selector: 'app-terms',
   standalone: true,
@@ -10,7 +12,28 @@ import {TranslatePipe} from "@ngx-translate/core";
   templateUrl: './terms.component.html',
   styleUrl: './terms.component.css'
 })
-export class TermsComponent {
+export class TermsComponent implements AfterViewInit {
   effectiveDate = '2025-09-01';
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private scroller: ViewportScroller
+  ) {}
 
+  ngAfterViewInit() {
+    setTimeout(() => this.scrollFromFragment(), 0);
+
+    this.router.events.pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(() => setTimeout(() => this.scrollFromFragment(), 0));
+  }
+
+  private scrollFromFragment() {
+    const frag = this.route.snapshot.fragment;
+    if (frag) this.scroller.scrollToAnchor(frag);
+  }
+
+  scrollTo(id: string, ev: Event) {
+    ev.preventDefault();
+    this.scroller.scrollToAnchor(id);
+  }
 }
